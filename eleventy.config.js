fd5f7ffde8d4597ded156bd50a6b73d1b1e761d9
Erik Nordberg <erik.j.nordberg@gmail.com>
2026-08-06 22:41:14 +0200

    Self-hostade typsnitt och versionerad CSS-URL

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // Kopiera CSS och bilder rakt igenom till den färdiga sajten
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/bilder");
+  eleventyConfig.addPassthroughCopy("src/fonter");
 
   // Gör datum läsbara i mallarna: 2026-08-06 -> 6 augusti 2026
   eleventyConfig.addFilter("datum", (value) =>
